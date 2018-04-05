bb95fe9142913fb9b9fd01150c102b02969b72bb
Christopher Giustiniani <giustiniani.christopher@gmail.com>
2018-04-05 20:12:02 +0200

    initial commit projectX server

---
+++ server/src/main/java/io/javabrains/springbootstarter/user/UserRepository.java
@@ -0,0 +1,9 @@
+package io.javabrains.springbootstarter.user;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, String> {
+
+
+
+}
